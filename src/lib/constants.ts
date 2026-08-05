93677343ebedad503371793fee3bdbc25915a496
yuhoa0070-pixel <yuhoa0070@gmail.com>
2026-08-05 18:51:32 +0700

    Update Mountain air card image

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -45,6 +45,7 @@ export const heroGalleryData: HeroGalleryCard[] = [
   {
     label: 'Mountain air',
     seed: 'mountain-air',
+    image: '/mountain-air.png',
     icon: '<path d="M3 19l6-10 4 6 2-3 6 7z"/>',
   },
   {
